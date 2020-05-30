bea0e5c3a568d02d32ae50570e0816a74e58410f
Simulacrum <simulacrum.live@gmail.com>
2020-05-30 19:10:40 +0000

    Added OSX inputs

--- wallet-compile/compile-setup.sh
+++ wallet-compile/compile-setup.sh
@@ -19,5 +19,12 @@ sudo apt-get install apt-cacher-ng
 #Run the compile environment setup
 ./gitian-build.py --docker --setup
 
+
+#add the macos required inputs for the gitian builder
+wget https://github.com/simulacrum-trade/simulacrum/raw/master/wallet-compile/MacOSX10.11.sdk.tar.gz
+wget https://github.com/simulacrum-trade/simulacrum/raw/master/wallet-compile/osslsigncode-1.7.1.tar.gz
+mkdir gitian-builder/inputs && cp MacOSX10.11.sdk.tar.gz gitian-builder/inputs/
+cp osslsigncode-1.7.1.tar.gz gitian-builder/inputs/
+
 #Run the compile
 ./gitian-build.py --docker -b -c -o w,l,m --detach-sign simulacrum master
